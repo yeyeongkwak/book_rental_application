5ac072f7293155b2931612235c2ef349b0419440
aesop0817 <aesop0817@douzone.com>
2023-08-29 00:45:55 +0900

    08.29- 스케줄러로 남은 일 수 차감하는 것까지 개발

--- src/main/kotlin/com/group/book_application/application/usecase/schedule/ScheduleTasks.kt
+++ src/main/kotlin/com/group/book_application/application/usecase/schedule/ScheduleTasks.kt
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component
 class ScheduleTasks(private val executor: CalculateLeftDateExecutor) {
 
         private val log= LoggerFactory.getLogger(ScheduleTasks::class.java)
-    @Scheduled(cron = "0 20 0 * * *")
+    @Scheduled(cron = "0 0 0 * * *")
     fun calculateLeftDays() {
         executor.subRentDate()
 //            .apply { executor.subPoint(memberId)}  //suspend가 걸리면 Scheduled는 파라미터가 없어야 한다고 빌드가 안됨;; 왜지?ㅠ
